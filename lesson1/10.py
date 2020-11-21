7b5e70e1429752995432d06a8a75479f7963873a
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-11-21 14:45:36 +0600

    Sdsdsds

---
+++ lesson1/10.py
@@ -0,0 +1,10 @@
+a = input()
+b = input()
+aInt = int(a)
+bInt = int(b)
+s = aInt * bInt
+outputS = f"Прямоугольник со сторонами {a} и {b} площадь равна {s}"
+print(outputS)
+p = 2 * (aInt + bInt)
+outputP = f"Прямоугольник со сторонами {a} и {b} периметр равен {p}"
+print(outputP)
