38da335915a126b2e6a116459ce5cfec0c277be2
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-11-22 18:27:37 +0600

    sdsdsdsds

--- lesson2/7.py
+++ lesson2/7.py
@@ -1,4 +1,27 @@
-введите свое имя:Ерасыл
-введите год рождения:1998
-
-Вас зовут Ерасыл и вам сейчас 22
+name = input("введите свое имя:")
+if name != "":
+    if not name.isalpha():
+        print("Please write only alphabets characters")
+        exit()
+else:
+    print("Please write something")
+    exit()
+year = input("введите год рождения:")
+if year != "":
+    if year.isnumeric():
+        year = int(year)
+    else:
+        print("Please write only numbers")
+        exit()
+else:
+    print("Please write something")
+    exit()
+age = 2020 - year
+output = f"Вас зовут {name} и вам сейчас {age}"
+print(output)
+# name.isalpha()->для букв
+# name.isnumeric()->для цифр
+# введите свое имя:Ерасыл
+# введите год рождения:1998
+#
+# Вас зовут Ерасыл и вам сейчас 22
---
+++ lesson2/8.py
@@ -0,0 +1,13 @@
+a = 6
+b = 5
+c = a > b
+print(c)
+print(not c)
+# if not a > b:
+#     print("a<=b")
+# else:
+#     print("a>b")
+# if a > b:
+#     print("a>b")
+# else:
+#     print("a<=b")
