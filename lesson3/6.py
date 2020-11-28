d4a15eba7d0e2a4dfedf559b150f4a6be0cb4b9a
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-11-28 13:39:44 +0600

    sdsd

---
+++ lesson3/6.py
@@ -0,0 +1,20 @@
+age = 0
+end = 100
+while age <= end:
+    print("дело1")
+    print("дело2")
+    print("дело3")
+    age = age + 1
+    print(f"ДР {age}")
+
+# counter -> возраст,0(когда день рождение +1)
+# end -> 100
+#
+# counter <= end
+#
+#
+# цикл counter<=end
+#     дело1
+#     дело2
+#     дело3
+#     counter = counter + 1
---
+++ lesson3/7.py
@@ -0,0 +1,10 @@
+i = 0
+n = 5
+while i < n:
+    print("Hello")
+    i += 1
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
