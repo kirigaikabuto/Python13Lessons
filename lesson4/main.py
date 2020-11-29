569b8c1599afc1267e77bf7d951bab295d75a403
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-11-29 11:11:45 +0600

    sdsd

---
+++ lesson4/main.py
@@ -0,0 +1,21 @@
+i = 0
+n = 5
+sumi = 0
+while i < n:
+    sumi = sumi + i
+    i += 1
+
+print(sumi)
+# i = 0
+# sumi = 0
+# sumi = 0 + 0
+# print(sumi)
+#
+# i = 1
+# sumi = 0
+# sumi = 0 + 1
+# print(sumi)
+# i = 2
+# sumi = 1
+# sumi = 1 + 2
+# print(sum)
