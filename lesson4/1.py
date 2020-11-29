bbf94cc4c5abb01cbc4a05f0b0af5396474ee548
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-11-29 11:18:25 +0600

    sdsd

---
+++ lesson4/1.py
@@ -0,0 +1,6 @@
+i = int(input("начало:"))
+n = int(input("конец:"))
+while i < n:
+    if i % 2 == 0:
+        print(i, end=" ")
+    i += 1
---
+++ lesson4/2.py
@@ -0,0 +1,5 @@
+s = ""
+while s != "!":
+    s = input()
+
+print("end of cycle")
