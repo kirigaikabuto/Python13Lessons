04dd42b653ad19aaeea96392e168cc076a5876e1
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-12-05 13:32:27 +0600

    sdsd

--- lesson5/operations.py
+++ lesson5/operations.py
@@ -4,10 +4,21 @@ c = 6
 formula = a * (a + (b * c / a))
 print(formula)
 
-#1) a*b
+# 1) a*b
 # a = 3
 # b = 4
-
+# 2) (a+b)*(a+c)
+# a = 3
+# b=  4
+# c = 7
+# 3) (a+b)*(c*(c+d))
+# a = 3
+# b = 4
+# c = 7
+# d = 8
+# 4) (a+b)^2
+# a =2
+# b =4
 
 
 # print(3 + 4)
