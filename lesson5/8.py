7b2fed547bd9e7f6d7593fd93164d710fa3c9cc4
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-12-05 14:31:18 +0600

    sdsd

--- lesson5/8.py
+++ lesson5/8.py
@@ -1,6 +1,15 @@
 initial = int(input())
 end = int(input())
 sumi = 0
+if initial % 2 != 0:
+    initial += 1
+if end % 2 != 0:
+    end -= 1
 for i in range(initial, end + 1, 2):
     print(i)
     sumi = sumi + i
+
+# 3 10
+# 4  6 8 10
+# #5 20
+# 6 8 10 12 14
