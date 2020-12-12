8ce70a80adb31872e90160005f27aafeb3bf21df
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-12-12 14:40:39 +0600

    sdsd

--- lesson7/5.py
+++ lesson7/5.py
@@ -8,3 +8,6 @@ os.chdir(desktopPath)  # cd /home/kirigaikabuto/Desktop
 os.mkdir("newFOlder")  # mkdir newFolder
 newPath = os.getcwd()  # pdw
 print(newPath)
+
+# создать папку на рабочем столе(desktopFolder) и внутри нее создать еще три папки(1,2,3)
+# после создания папки на рабочем столе необходимо поменять путь в котором вы находитесь
