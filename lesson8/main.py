35deebcfe6b940d47e5557213b4689758d75a31e
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-12-13 13:32:56 +0600

    sdsd

--- lesson8/main.py
+++ lesson8/main.py
@@ -5,6 +5,8 @@ os.chdir(pathToDesktop)  # cd pathDoDesktop
 mainFileName = "folderLesson8"
 # os.mkdir(mainFileName)  # mkdir mainFileName
 os.chdir(mainFileName)
-os.mkdir("1")
-os.mkdir("2")
-os.mkdir("3")
+# os.mkdir("1")
+# os.mkdir("2")
+# os.mkdir("3")
+os.chdir("1")
+os.mknod("1.txt")
