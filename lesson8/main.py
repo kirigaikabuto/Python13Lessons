64eca741b28e615fed8dbb113f5c2978b3816171
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-12-13 13:13:54 +0600

    sdsd

---
+++ lesson8/main.py
@@ -0,0 +1,10 @@
+import os
+
+pathToDesktop = "/home/kirigaikabuto/Desktop"
+os.chdir(pathToDesktop)  # cd pathDoDesktop
+mainFileName = "folderLesson8"
+# os.mkdir(mainFileName)  # mkdir mainFileName
+os.chdir(mainFileName)
+os.mkdir("1")
+os.mkdir("2")
+os.mkdir("3")
