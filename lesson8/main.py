e431fa79354b4d5b9d8a895f463b871aebc3884b
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-12-13 13:50:17 +0600

    sdsd

--- lesson8/main.py
+++ lesson8/main.py
@@ -3,10 +3,27 @@ import os
 pathToDesktop = "/home/kirigaikabuto/Desktop"
 os.chdir(pathToDesktop)  # cd pathDoDesktop
 mainFileName = "folderLesson8"
-# os.mkdir(mainFileName)  # mkdir mainFileName
+if not os.path.isdir(mainFileName):
+    os.mkdir(mainFileName)  # mkdir mainFileName
 os.chdir(mainFileName)
-# os.mkdir("1")
-# os.mkdir("2")
-# os.mkdir("3")
-os.chdir("1")
-os.mknod("1.txt")
+file1Name = "1"
+file2Name = "2"
+file3Name = "3"
+if not os.path.isdir(file1Name):
+    os.mkdir(file1Name)
+else:
+    print(file1Name + " already created")
+if not os.path.isdir(file2Name):
+    os.mkdir(file2Name)
+else:
+    print(file2Name + " already created")
+if not os.path.isdir(file3Name):
+    os.mkdir(file3Name)
+else:
+    print(file3Name + " already created")
+os.chdir(file1Name)
+messageToFile = "Yerassyl"
+textFileName = "1.txt"
+file = open(textFileName, "w")  # w-write, r - read
+file.write(messageToFile)
+file.close()
