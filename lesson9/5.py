c04090b05717642c9e0994c6b07573003eaf2052
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-12-19 13:36:17 +0600

    sdsd

---
+++ lesson9/5.py
@@ -0,0 +1,9 @@
+# s = "Я и он asdasd иsdsds sdsdsd"
+# words = s.split("и")
+# print(words)
+word = "It is ! time ! for game"
+# word1 = "It is "
+# word2 = " time "
+# word3 = " for game"
+words = word.split("!")
+print(words)
