c4811cb9c21068b3ed0fc27c4d16c6170f8862ad
Yerassyl Tleugazy <ytleugazy@dar.kz>
2021-01-07 14:18:36 +0600

    sdsd

--- lesson11/4.py
+++ lesson11/4.py
@@ -5,6 +5,7 @@
 # 4) условие на выбор авторизаци
 # 5) запросы на вводе username и password
 # 6) пройтись циклом по всем данным и если был найден пользователь вывести соотвествующее сообщение
+# 7) сделать проверку по username и если такого человека нету то написать соотствующеее
 # 1
 import json
 
@@ -18,12 +19,24 @@ print("[1]Авторизация")
 # 4
 number = input("выбор:")
 if number == "1":
+    isUsername = False
+    isPassword = False
     print("---АВТОРИЗАЦИЯ---")
     # 5
     username = input("username:")
     password = input("password:")
     # 6
     for i in users:
-        if i["username"] == username and i["password"] == password:
-            print(f"Добро пожаловать {username}")
+        if i["username"] == username:
+            isUsername = True
+            if i["password"] == password:
+                isPassword = True
             break
+
+    if isUsername:
+        if isPassword:
+            print(f"Добро пожаловать {username}")
+        else:
+            print("Неправильный пароль")
+    else:
+        print("У нас нет такого пользователя")
