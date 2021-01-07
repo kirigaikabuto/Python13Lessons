3db5a3c630c7b08fff405caf767265c100d5f9d7
Yerassyl Tleugazy <ytleugazy@dar.kz>
2021-01-07 13:45:58 +0600

    sdsd

--- lesson11/2.py
+++ lesson11/2.py
@@ -1,5 +1,22 @@
-book1 = {
-    "name": "book1",
-    "price": 200
+# 1) считать данные из файла
+# 2) преобразовать данные обратно в массив
+# 3) добавить новый элемент в массив
+# 4) сохранить данные в файл
+# 1
+import json
+file = open("data.json", "r")
+peoplesJson = file.read()
+file.close()
+# 2
+peoples = json.loads(peoplesJson)
+# 3
+people = {
+    "name": "name4",
+    "age": 222
 }
-books = [book1]
+peoples.append(people)
+# 4
+peoplesJson = json.dumps(peoples, indent=4)
+file = open("data.json", "w")
+file.write(peoplesJson)
+file.close()
