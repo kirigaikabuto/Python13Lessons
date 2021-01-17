701a6af431bf5be882f89611c22ce1c92b30b88e
Yerassyl Tleugazy <ytleugazy@dar.kz>
2021-01-17 10:26:46 +0600

    sdsd

---
+++ lesson14/main.py
@@ -0,0 +1,21 @@
+def getWordsFromArr(arr):
+    words = []
+    for i in arr:
+        if i.isalpha():
+            words.append(i)
+    return words
+
+
+def getNumbersFromArr(arr):
+    numbers = []
+    for i in arr:
+        if i.isnumeric():
+            numbers.append(i)
+    return numbers
+
+
+arr = ["sdssdsd", "2", "Hello", "4", "5", "sdsd123", "dsd2323"]
+words = getWordsFromArr(arr)
+numbers = getNumbersFromArr(arr)
+print(words)
+print(numbers)
