1164227e3dd099ec83893299cf852d16653e2bc3
Yerassyl Tleugazy <ytleugazy@dar.kz>
2021-01-24 10:50:35 +0600

    sdsd

---
+++ lesson16/2.py
@@ -0,0 +1,70 @@
+groups = [
+    {
+        "id": 1,
+        "name": "it1601",
+    },
+    {
+        "id": 2,
+        "name": "it1701",
+    }
+]
+
+students = [
+    {
+        "id": 1,
+        "first_name": "yerassyl",
+        "last_name": "tleugazy",
+        "email": "tleugazy98@gmail.com",
+        "phone": "87086394516",
+        "username": "sdsddsd",
+        "password": "asdsasds",
+        "group_id": 2,
+    },
+    {
+        "id": 2,
+        "first_name": "anel",
+        "last_name": "tleugazy",
+        "email": "anel98@gmail.com",
+        "phone": "87086394516",
+        "group_id": 1,
+    },
+]
+
+teachers = [
+    {
+        "id": 1,
+        "first_name": "teacher1",
+        "last_name": "teacher1",
+        "email": "teacher1@gmail.com",
+        "phone": "87086394516",
+
+    },
+    {
+        "id": 2,
+        "first_name": "anel",
+        "last_name": "tleugazy",
+        "email": "anel98@gmail.com",
+        "phone": "87086394516",
+    },
+]
+
+lessons = [
+    {
+        "id": 1,
+        "name": "math"
+    },
+    {
+        "id": 2,
+        "name": "algebra"
+    }
+]
+
+schedule = [
+    {
+        "id": 1,
+        "group_id": 2,
+        "lesson_id": 1,
+        "teacher_id": 2,
+        "time": "24.01.2021",
+    },
+]
