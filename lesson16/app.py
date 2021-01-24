ad256cad455354ca5375492c602cf9eac91f781f
Yerassyl Tleugazy <ytleugazy@dar.kz>
2021-01-24 11:50:25 +0600

    sdsd

--- lesson16/app.py
+++ lesson16/app.py
@@ -17,13 +17,29 @@ def studentStart():
         studentStart()
 
 
+def teacherStart():
+    print("[1]Login")
+    print("[2]Register")
+    print("[3]Back")
+    num = input()
+    if num == "1":
+        pass
+    elif num == "2":
+        pass
+    elif num == "3":
+        start()
+    else:
+        print("There is no choice")
+        teacherStart()
+
+
 def start():
     print("[1]Teacher")
     print("[2]Student")
     print("[3]Exit")
     num = input()
     if num == "1":
-        pass
+        teacherStart()
     elif num == "2":
         studentStart()
     elif num == "3":
