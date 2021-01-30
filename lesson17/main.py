4862c749bec52c83350b7be1ee65536f2603eba5
yerassyl <tleugazy98@gmail.com>
2021-01-30 14:59:30 +0600

    sdsd

--- lesson17/main.py
+++ lesson17/main.py
@@ -3,3 +3,8 @@ import pymongo
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["yerassyldb"]
 collection = db["products"]
+d = {
+    "name": "product4",
+    "price": 15
+}
+collection.insert_one(d)
