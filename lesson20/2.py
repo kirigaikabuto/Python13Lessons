00e9979c6e27352d5df6c965c66fa0c0c32f27b4
kirigaikabuto <tleugazy98@gmail.com>
2021-02-07 13:50:31 +0600

    sdsd

--- lesson20/2.py
+++ lesson20/2.py
@@ -9,6 +9,15 @@ class Triangle:
         s = f"A={self.a},B={self.b},C={self.c}"
         print(s)
 
+    def calculateArea(self):
+        print("calculate areal")
+
+    def calculatePerimeter(self):
+        print(self.a + self.b + self.c)
+
+    def createInMongoDb(self):
+        pass
+
 
 st1 = int(input("A="))
 st2 = int(input("B="))
@@ -16,3 +25,7 @@ st3 = int(input("C="))
 print("start program")
 # object created
 tr1 = Triangle(a=st1, b=st2, c=st3)
+tr1.show()
+tr1.calculateArea()
+tr1.calculatePerimeter()
+tr1.createInMongoDb()
