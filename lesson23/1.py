44bd4cd98e0669fa21d81ccaafd6d2bf13f8bc6b
kirigaikabuto <tleugazy98@gmail.com>
2021-02-21 13:21:58 +0600

    sdsd

--- lesson23/1.py
+++ lesson23/1.py
@@ -1,6 +1,7 @@
 # from models.person import Person
 # from models.cat import Cat
-from models import Person, Cat
+# from models import Person, Cat
+from models import *
 
 p1 = Person(name="yerassyl", age=23)
 p1.print_info()
