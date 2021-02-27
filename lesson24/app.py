63392c185563e78d68578d75e7742007f71e5ea1
kirigaikabuto <tleugazy98@gmail.com>
2021-02-27 15:17:42 +0600

    sdsd

--- lesson24/app.py
+++ lesson24/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
+products = [
+    "product1",
+    "product2",
+    "product3"
+]
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main_page():
-    return "Hello"
+    message = "Hello my name is Yerassyl"
+    return render_template("main/main_page.html", mess=message)
 
 
 if __name__ == "__main__":
