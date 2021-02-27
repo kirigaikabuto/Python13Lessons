b3b5d7dea976b1c17417f93304e8358318123e17
kirigaikabuto <tleugazy98@gmail.com>
2021-02-27 15:19:13 +0600

    sdsd

--- lesson24/app.py
+++ lesson24/app.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 @app.route("/")
 def main_page():
     message = "Hello my name is Yerassyl"
-    return render_template("main/main_page.html", mess=message)
+    name = "yerassyl"
+    return render_template("main/main_page.html", mess=message, name=name)
 
 
 if __name__ == "__main__":
