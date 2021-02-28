51d33aaa215f68c31cafcab6f6b5c62e965d1cf8
kirigaikabuto <tleugazy98@gmail.com>
2021-02-28 11:45:04 +0600

    sdsd

---
+++ lesson25/5.py
@@ -0,0 +1,12 @@
+# Предложите пользователю ввести 10 чисел с помощью цикла. Найдите и распечатайте среднее значение 10 чисел.
+# numbers = []
+# i = 0
+# while i < 10:
+#     number = int(input())
+#     numbers.append(number)
+#     i += 1
+# sumi = 0
+# n = len(numbers)
+# for i in numbers:
+#     sumi += i
+# print(sumi / n)
