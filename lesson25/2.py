4d96e281fc572f4614c2118170723f305d1cc9fa
kirigaikabuto <tleugazy98@gmail.com>
2021-02-28 12:01:28 +0600

    sdsd

--- lesson25/2.py
+++ lesson25/2.py
@@ -1,5 +1,30 @@
-#создать таблицу products
+import sqlite3
 
-id
-name
-price
+connection = sqlite3.connect("mydatabase.db")
+cursor = connection.cursor()
+sql = """
+create table if not exists products(
+    id int,
+    name text,
+    price int
+)
+"""
+# id = 1
+# name = "product1"
+# price = 300
+# cursor.execute(sql)
+# sql_insert = """
+# insert into products
+# (id, name, price)
+# values
+# (?,?,?)
+# """
+# cursorInsert = connection.cursor()
+# cursorInsert.execute(sql_insert, [id, name, price])
+# connection.commit()
+selectSql = "select * from products"
+cursorSelect = connection.cursor()
+cursorSelect.execute(selectSql)
+data = cursorSelect.fetchall()
+for i in data:
+    print(i)
