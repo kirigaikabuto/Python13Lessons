1c3b79c8681f0d45bea5ab6f12eb0accbf32d006
Yerassyl <tleugazy98@gmail.com>
2021-03-14 14:36:14 +0600

    sdsd

--- lesson27/blog/views.py
+++ lesson27/blog/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render, HttpResponse
 
 
 def main_page(request):
-    return HttpResponse("hello from main page")
+    name = "yerassyl"
+    articles = [
+        {
+            "name": "article1",
+            "description": "it is the best article",
+        },
+        {
+            "name": "article2",
+            "description": "it is the best article",
+        }
+    ]
+    d = {
+        "first_name": name,
+        "articles": articles,
+    }
+
+    return render(request, "blog/main_page.html", context=d)
