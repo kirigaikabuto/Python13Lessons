f2b9da0a1aacd1ef7a76e346d472244275a5179d
Yerassyl <tleugazy98@gmail.com>
2021-03-14 14:45:31 +0600

    sdsd

--- lesson27/blog/models.py
+++ lesson27/blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.CharField(max_length=255)
