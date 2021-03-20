5af79f30b6197bb07963d09dfc60e92ef31138e6
Yerassyl <tleugazy98@gmail.com>
2021-03-20 19:15:10 +0600

    sdsd

--- lesson28part2/todos/urls.py
+++ lesson28part2/todos/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path("", main_page, name="main_page"),
     path("detail/<int:id>/", detail_page, name="detail_page"),
-    path("add_todo_action/", add_todo_action, name="add_todo_action")
+    path("add_todo_action/", add_todo_action, name="add_todo_action"),
+    path("remove/<int:id>/", remove_todo_action, name="remove_todo_action"),
+    path("update/", update_todo_action, name="update_todo_action"),
 ]
--- lesson28part2/todos/views.py
+++ lesson28part2/todos/views.py
@@ -3,9 +3,11 @@ from .models import *
 
 
 def main_page(request):
-    todos = Todo.objects.all()
+    todos_not_completed = Todo.objects.all().filter(isCompleted=False)
+    todos_completed = Todo.objects.all().filter(isCompleted=True)
     d = {
-        "todos": todos,
+        "todos_not_completed": todos_not_completed,
+        "todos_completed": todos_completed,
     }
     return render(request, "todos/main_page.html", context=d)
 
@@ -24,5 +26,20 @@ def add_todo_action(request):
     title = request.POST["title"]
     deadline = request.POST["deadline"]
     todo = Todo(title=title, deadline=deadline)
+    todo.save()
+    return redirect("main_page")
+
+
+def remove_todo_action(request, id):
+    instance = Todo.objects.get(pk=id)
+    instance.delete()
+    return redirect("main_page")
+
+
+def update_todo_action(request):
+    id = int(request.POST["id"])
+    isCompleted = bool(request.POST["isCompleted"])
+    todo = Todo.objects.get(pk=id)
+    todo.isCompleted = isCompleted
     todo.save()
     return redirect("main_page")
