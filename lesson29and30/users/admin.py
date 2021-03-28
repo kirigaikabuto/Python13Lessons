d17c18b1a308979e8f1e7b98c39f972802bc0f79
Yerassyl <tleugazy98@gmail.com>
2021-03-28 13:10:03 +0600

    sdsd

--- lesson29and30/users/admin.py
+++ lesson29and30/users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Log)
+
+class LogAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_time"
+    list_display = ("user", "action", "created_time")
+
+
+admin.site.register(Log, LogAdmin)
