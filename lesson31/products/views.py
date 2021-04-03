0a4493fc2bf836e420de57718f117b741319e4c1
Yerassyl <tleugazy98@gmail.com>
2021-04-03 19:32:52 +0600

    sdsd

--- lesson31/products/views.py
+++ lesson31/products/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 def main_page(request):
     return render(request, "products/main_page.html")
+
+
+def contact_page(request):
+    return render(request, "products/contacts.html")
