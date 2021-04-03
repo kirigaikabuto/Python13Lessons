fde3ca0c00309a661c3bc74a60ab8290b1d2ae66
Yerassyl <tleugazy98@gmail.com>
2021-04-03 19:30:48 +0600

    sdsd

--- lesson31/products/views.py
+++ lesson31/products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
 
-def main_page():
-    return HttpResponse("Hello from main page")
+def main_page(request):
+    return render(request, "products/main_page.html")
