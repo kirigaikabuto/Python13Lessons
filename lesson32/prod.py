7440ec07628927f97cb77eab06c27908207fddff
Yerassyl <tleugazy98@gmail.com>
2021-04-11 09:50:55 +0600

    sdsd

---
+++ lesson32/prod.py
@@ -0,0 +1,7 @@
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
+producer.send("python_lesson", b'Hello from kafak')
+metrics = producer.metrics()
+print(metrics)
+producer.flush()
