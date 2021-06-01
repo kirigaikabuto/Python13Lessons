1471ee58ea775f171080897cf52944ce5cb7d461
Yerassyl <tleugazy98@gmail.com>
2021-06-01 14:16:06 +0600

    sdsd

--- lesson35kf/kf_cons.py
+++ lesson35kf/kf_cons.py
@@ -1,8 +1,16 @@
 from kafka import KafkaConsumer
 import json
+import pymongo
 
+client = pymongo.MongoClient("localhost", 27017)
+db = client["quiz_app_logs"]
+loginCollection = db["login_logs"]
+createQuizCollection = db["create_quiz_logs"]
 consumer = KafkaConsumer("quiz_app")
 for msg in consumer:
     jsonData = msg.value
     d = json.loads(jsonData)
-    print(d["username"])
+    if d["action"] == "login":
+        loginCollection.insert_one(d)
+    elif d["action"] == "create_quiz":
+        createQuizCollection.insert_one(d)
