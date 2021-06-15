e8d816d472e8da59cf4ab71cbb97245c4409a185
Yerassyl <tleugazy98@gmail.com>
2021-06-15 21:06:40 +0600

    sdsdds

---
+++ LessonRuslan1/9.py
@@ -0,0 +1,21 @@
+grades = [
+    ["name1", "name2", "name3"],
+    ["name4", "name5", "name6"],
+    ["name7", "name8", "name9"],
+]
+for i in range(len(grades)):
+    print(i, grades[i])
+
+archive = [
+    ["yerassyl", "tleugazy", 23],
+    ["anel", "aidarbek", 21],
+    ["ruslan", "zurufov", 22]
+]
+
+for i in archive:
+    print(i)
+names = ["yerassyl", "anel", "ruslan"]
+surnames = ["tleugayz", "aidarbek", "zurufov"]
+ages = ["23", "21", "22"]
+for i in range(3):
+    print(names[i], surnames[i], ages[i])
